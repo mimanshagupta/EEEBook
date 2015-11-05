d40c099c1e35a9a4bb59d5755a3427cbfd700d59
mimanshagupta <mimansha_gupta@hotmail.com>
2015-11-05 22:54:12 +0800

    Project files added

---
+++ app/src/main/java/snp/com/eeebook/MainActivity.java
@@ -0,0 +1,16 @@
+package snp.com.eeebook;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Mimansha on 3/11/2015.
+ */
+public class MainActivity extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
