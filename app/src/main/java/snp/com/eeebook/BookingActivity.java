baf80fc808804c5352934fc695a9610a9101631a
mimanshagupta <mimansha_gupta@hotmail.com>
2015-11-08 18:21:14 +0800

    timePickers added and material action bar added, some enhancements to layouts as well

---
+++ app/src/main/java/snp/com/eeebook/BookingActivity.java
@@ -0,0 +1,159 @@
+package snp.com.eeebook;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by Mimansha on 6/11/2015.
+ */
+public class BookingActivity extends ActionBarActivity implements AdapterView.OnItemSelectedListener, DatePicker.OnDateChangedListener, View.OnClickListener, TimePicker.OnTimeChangedListener{
+    private Spinner spinner1;
+    private TextView roomDate, roomDateLabel, roomTimelabel, roomTime;
+    private Button dateSelect, timeSelect, makeBooking;
+    private Toolbar toolbar;
+    private TimePicker timePicker, timePicker2;
+    private int fromhod = 0, frommin = 0, tohod = 0, tomin = 0;
+
+    @Override
+    public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        roomDateLabel.setVisibility(View.VISIBLE);
+        roomDate.setVisibility(View.VISIBLE);
+        monthOfYear++;
+        roomDate.setText(dayOfMonth + " / " + monthOfYear + " / " + year );
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        if(spinner1.getSelectedItemId() == 1) {
+            dateSelect.setVisibility(View.VISIBLE);
+            dateSelect.setOnClickListener(BookingActivity.this);
+            timeSelect.setVisibility(View.VISIBLE);
+            timeSelect.setOnClickListener(BookingActivity.this); //check listener
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking);
+
+        addItemsOnSpinner1();
+        //addListenerOnSpinnerItemSelection();
+        toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
+        setSupportActionBar(toolbar);
+        roomDate = (TextView) findViewById(R.id.roomDate);
+        roomDate.setVisibility(View.INVISIBLE);
+        dateSelect = (Button) findViewById(R.id.dateSelect);
+        dateSelect.setVisibility(View.INVISIBLE);
+        roomDateLabel = (TextView) findViewById(R.id.roomDatelabel);
+        roomDateLabel.setVisibility(View.INVISIBLE);
+        timeSelect = (Button) findViewById(R.id.fromTime);
+        timeSelect.setVisibility(View.INVISIBLE);
+        roomTimelabel = (TextView) findViewById(R.id.roomTimelabel);
+        roomTimelabel.setVisibility(View.INVISIBLE);
+        roomTime = (TextView) findViewById(R.id.roomTime);
+        roomTime.setVisibility(View.INVISIBLE);
+        makeBooking = (Button) findViewById(R.id.makeBooking);
+    }
+
+    public void addItemsOnSpinner1() {
+
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+        List<String> list = new ArrayList<String>();
+        list.add("Select Component to Book");
+        list.add("Room");
+        list.add("Equipment");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(dataAdapter);
+        spinner1.setSelection(0);
+        spinner1.setPrompt("Select Component to Book");
+        spinner1.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.d("mushu", "Reached onClick");
+        if(v.getId()== R.id.dateSelect) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            DatePicker picker = new DatePicker(this);
+            picker.setCalendarViewShown(false);
+            builder.setTitle("Select Date");
+            builder.setView(picker);
+            builder.setNegativeButton("Cancel", null);
+            builder.setPositiveButton("Set", null);
+
+            Calendar calendar = Calendar.getInstance();
+            picker.init(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), BookingActivity.this);
+
+            builder.show();
+        }
+
+        else if (v.getId() == R.id.fromTime) {
+            Log.d("mushu", "Reached onClick for Time");
+            AlertDialog.Builder timeDialog = new AlertDialog.Builder(this);
+            timePicker = new TimePicker(this);
+            timeDialog.setTitle("Select Start Time");
+            timeDialog.setView(timePicker);
+            timeDialog.setNegativeButton("Cancel", null);
+            timeDialog.setPositiveButton("Set", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    AlertDialog.Builder timeDialog2 = new AlertDialog.Builder(BookingActivity.this);
+                    timePicker2 = new TimePicker(BookingActivity.this);
+                    timeDialog2.setTitle("Select End Time");
+                    timeDialog2.setView(timePicker2);
+                    timeDialog2.setNegativeButton("Cancel", null);
+                    timeDialog2.setPositiveButton("Set", null);
+                    timePicker2.setOnTimeChangedListener(BookingActivity.this);
+                    timeDialog2.show();
+                }
+            });
+            timePicker.setOnTimeChangedListener(BookingActivity.this);
+            timeDialog.show();
+        }
+    }
+
+    @Override
+    public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+
+        if(view.equals(timePicker)) {
+
+            fromhod = hourOfDay;
+            frommin = minute;
+
+        } else if(view.equals(timePicker2)) {
+            roomTimelabel.setVisibility(View.VISIBLE);
+            tohod = hourOfDay;
+            tomin = minute;
+        }
+        roomTime.setText(new StringBuilder().append("From - ").append(fromhod).append(" : ").append(frommin).append(" To - ").append(tohod).append(" : ").append(tomin));
+        roomTime.setVisibility(View.VISIBLE);
+    }
+}
--- app/src/main/java/snp/com/eeebook/MainActivity.java
+++ app/src/main/java/snp/com/eeebook/MainActivity.java
@@ -1,16 +1,33 @@
 package snp.com.eeebook;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by Mimansha on 3/11/2015.
  */
-public class MainActivity extends Activity{
+public class MainActivity extends ActionBarActivity{
+
+    private Toolbar toolbar;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
+        setSupportActionBar(toolbar);
     }
 
+
+
+    public void startbooking(View view){
+        Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+        Intent i = new Intent(MainActivity.this, BookingActivity.class);
+        startActivity(i);
+    }
 }
